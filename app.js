dfa8901013024adb874fcde34c1f1c3ee49ac6c9
Tony Cioara <tonyangelo9707@gmail.com>
2018-09-16 15:02:25 -0700

    Trying to fix new heroku error

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
     })
 })
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
