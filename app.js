1c7de31f490f5e7b1ebc7d38705343805b798f32
Tony Cioara <tonyangelo9707@gmail.com>
2018-09-16 14:41:18 -0700

    Fixing port for heroku deploy

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.get('/', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('App listening on port 3000!');
-})
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 module.exports = app
