71dcab2e6a0b4d1ae86badef957c88e27b9104e3
Tony Cioara <tonyangelo9707@gmail.com>
2018-09-19 11:13:46 -0700

    users can now view trailers for movies that do have them

--- controllers/movies.js
+++ controllers/movies.js
@@ -11,7 +11,19 @@ module.exports = (app) => {
     app.get('/movies/:id', (req, res) => {
         moviedb.movieInfo({id: req.params.id})
             .then(movie => {
-                res.render('movies-show', {movie: movie});
+                if (movie.video) {
+                    moviesdb.movieVideos( {id:req.params.id}).then(videos => {
+                        movie.trailer_youtube_id = videos.results[0].key;
+                        renderTemplate(movie);
+                    });
+                } else {
+                    renderTemplate(movie);
+                };
+
+                function renderTemplate(movie)  {
+                    res.render('movies-show', { movie: movie });
+                };
+
             }).catch(console.error);
     });
 };
